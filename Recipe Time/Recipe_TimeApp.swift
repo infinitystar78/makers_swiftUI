4ac443c04bb14e57bce9ac87e49b74e9072ae777
Melvin W <melvinhw@me.com>
2022-11-03 10:06:04 +0000

    Initial Commit

---
+++ Recipe Time/Recipe_TimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Recipe_TimeApp.swift
+//  Recipe Time
+//
+//  Created by M W on 03/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct Recipe_TimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
